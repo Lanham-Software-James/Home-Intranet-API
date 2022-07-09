087d06ed88dff09878ef56d380026934a7163628
Jim Lanham <jrl5748@psu.edu>
2022-07-09 13:53:45 -0400

    Adding functions for check in and out. Cleaning up.

--- index.php
+++ index.php
@@ -1,22 +1,72 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
-
 require_once '../db/db.class.php';
 
 $app = AppFactory::create();
 
+
+/** 
+*** API Functions related to library
+**/
+$app->group('/library', function (RouteCollectorProxy $group) {
+
+  //Function to display number of books
+  $group->get('/count', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $q = json_encode($db->countBooks());
+    $response->getBody()->write($q);
+    return $response->withHeader('Content-Type', 'application/json');
+
+  });
+
+  //Function to list all the books and authors
+  $group->get('/list', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $q = json_encode($db->getBookAuthors());
+    $response->getBody()->write($q);
+    return $response->withHeader('Content-Type', 'application/json');
+
+  });
+
+  //Function to check in a book
+  $group->put('/checkin', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $params = $request->getQueryParams();
+
+    $db->checkInBook($params['bookID']);
+    
+    return $response;
+
+  });
+
+  //Function to check out a book
+  $group->put('/checkout', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $params = $request->getQueryParams();
+
+    $db->checkOutBook($params['bookID'], $params['name']);
+    
+    return $response;
+
+  });
+});
+
+//Options preflight
 $app->options('/{routes:.+}', function ($request, $response, $args) {
   return $response;
 });
 
+//CORS Configuration
 $app->add(function ($request, $handler) {
-
-  $CORSini = parse_ini_file('cors.ini');
-
   $response = $handler->handle($request);
   return $response
           ->withHeader('Access-Control-Allow-Origin', '*')
@@ -24,28 +74,11 @@ $app->add(function ($request, $handler) {
           ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
 
+//API Base
 $app->get('/', function (Request $request, Response $response, $args) {
 
   $response->getBody()->write('Home Intranet API Version 0.2');
   return $response;
-
-});
-
-$app->get('/countbooks', function (Request $request, Response $response, $args) {
-
-  $db = new DB();
-  $q = json_encode($db->countBooks());
-  $response->getBody()->write($q);
-  return $response->withHeader('Content-Type', 'application/json');
-
-});
-
-$app->get('/listbooks', function (Request $request, Response $response, $args) {
-
-  $db = new DB();
-  $q = json_encode($db->getBookAuthors());
-  $response->getBody()->write($q);
-  return $response->withHeader('Content-Type', 'application/json');
 
 });
 
