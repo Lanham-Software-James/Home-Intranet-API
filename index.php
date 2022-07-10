e2f6b198a7c189638325e82185aadf67788f7bd7
Jim Lanham <jrl5748@psu.edu>
2022-07-10 09:47:51 -0400

    Adding method to add a new book. Deleting a book is a WIP.

--- index.php
+++ index.php
@@ -9,56 +9,9 @@ require_once '../db/db.class.php';
 
 $app = AppFactory::create();
 
-
-/** 
-*** API Functions related to library
-**/
-$app->group('/library', function (RouteCollectorProxy $group) {
-
-  //Function to display number of books
-  $group->get('/count', function (Request $request, Response $response, $args) {
-
-    $db = new DB();
-    $q = json_encode($db->countBooks());
-    $response->getBody()->write($q);
-    return $response->withHeader('Content-Type', 'application/json');
-
-  });
-
-  //Function to list all the books and authors
-  $group->get('/list', function (Request $request, Response $response, $args) {
-
-    $db = new DB();
-    $q = json_encode($db->getBookAuthors());
-    $response->getBody()->write($q);
-    return $response->withHeader('Content-Type', 'application/json');
-
-  });
-
-  //Function to check in a book
-  $group->put('/checkin', function (Request $request, Response $response, $args) {
-
-    $db = new DB();
-    $params = $request->getQueryParams();
-
-    $db->checkInBook($params['bookID']);
-    
-    return $response;
-
-  });
-
-  //Function to check out a book
-  $group->put('/checkout', function (Request $request, Response $response, $args) {
-
-    $db = new DB();
-    $params = $request->getQueryParams();
-
-    $db->checkOutBook($params['bookID'], $params['name']);
-    
-    return $response;
-
-  });
-});
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
 
 //Options preflight
 $app->options('/{routes:.+}', function ($request, $response, $args) {
@@ -79,7 +32,77 @@ $app->get('/', function (Request $request, Response $response, $args) {
 
   $response->getBody()->write('Home Intranet API Version 0.2');
   return $response;
+});
 
+/** 
+*** API Functions related to library
+**/
+$app->group('/library', function (RouteCollectorProxy $group) {
+
+  //Function to display number of books
+  $group->get('/count', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $q = json_encode($db->countBooks());
+    $response->getBody()->write($q);
+    return $response->withHeader('Content-Type', 'application/json');
+  });
+
+  //Function to list all the books and authors
+  $group->get('/list', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $q = json_encode($db->getBookAuthors());
+    $response->getBody()->write($q);
+    return $response->withHeader('Content-Type', 'application/json');
+  });
+
+  //Function to check in a book
+  $group->put('/checkin', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $body = $request->getParsedBody();
+
+    $db->checkInBook($body['bookID']);
+    
+    return $response;
+  });
+
+  //Function to check out a book
+  $group->put('/checkout', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $body = $request->getParsedBody();
+
+    $db->checkOutBook($body['bookID'], $body['name']);
+    
+    return $response;
+  });
+
+  //Function to add a new book
+  $group->post('/add', function (Request $request, Response $response, $args) {
+
+    $db = new DB();
+    $body = $request->getParsedBody();
+    
+    $db->addBook($body['bookTitle'], $body['authorFirstName'], $body['authorMiddleName'], $body['authorLastName']);
+    
+    return $response;
+  });
+
+  /**
+  *** THIS NEEDS TO BE FIXED!!!
+  ***/
+  //Function to delete a book
+  // $group->delete('/delete', function (Request $request, Response $response, $args) {
+
+  //   $db = new DB();
+  //   $body = $request->getParsedBody();
+    
+  //   $db->deleteBook($body['bookID']);
+    
+  //   return $response;
+  // });
 });
 
 $app->run();
