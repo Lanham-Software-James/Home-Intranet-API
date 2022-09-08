01e9c29b8c9a528d79a1eef9dce918832a1ecbba
James Lanham <James.R.Lanham2021@gmail.com>
2022-09-08 15:38:20 -0400

    List files functionality.

--- routes/s3.php
+++ routes/s3.php
@@ -1,12 +1,9 @@
 <?php
-require_once __DIR__ .'/../../db/src/user.class.php';
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 use Aws\S3\S3Client;
-
-
 
 $app->group('/s3', function (RouteCollectorProxy $group) {
 
@@ -34,4 +31,67 @@ $app->group('/s3', function (RouteCollectorProxy $group) {
     
     return $response->withHeader('Content-Type', 'application/json');;
   });
+
+  //Function to get all files
+  $group->get('/objects', function (Request $request, Response $response, $args) {
+
+    //If file read
+    // $decoded = $request->getAttribute("token");
+    // $permissions = json_decode($decoded["scope"], true);
+
+    // if(!$permissions['hue_read']){
+    //   return $response->withStatus(401);
+    // }
+
+    $params = $request->getQueryParams();
+
+    $s3 = parse_ini_file(__DIR__ . '/../config/s3.ini');
+    $s3client = new S3Client(['region' => 'us-east-1', 'version' => 'latest', 'credentials' => ["key" => $s3['keyid'], "secret" => $s3['key']]]);
+   
+    $res = [];
+    $dirs = [];
+
+    $contents = $s3client->listObjects([
+      'Bucket' => $s3['bucket'],
+      'Prefix' => $params['location']
+    ]);
+
+    foreach ($contents['Contents'] as $content) {
+      if(strcmp($params['location'], $content['Key']) == 0){
+
+      }
+      else if(substr($content['Key'], -1) == '/' && isFile($dirs, $content['Key'])){
+        $fileName = explode('/', $content['Key']);
+        $fileCount = count($fileName) - 2;
+
+        array_push($dirs, $content['Key']);
+        array_push($res, $fileName[$fileCount] . "/");
+      } 
+      else if(isFile($dirs, $content['Key'])){
+        $fileName = explode('/', $content['Key']);
+        $fileCount = count($fileName) - 1;
+        
+        array_push($res, $fileName[$fileCount]);
+      }
+        
+    }
+
+    $res = json_encode($res);
+    
+    $response->getBody()->write($res);
+    
+    return $response->withHeader('Content-Type', 'application/json');;
+  });
 });
+
+function isFile($dirs, $content) {
+  $val = true;
+
+  foreach ($dirs as $key => $value) {
+    if(str_contains($content, $value)) {
+      $val = false;
+    }
+  }
+
+  return $val;
+}
